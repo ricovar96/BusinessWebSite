1b2645aff84bceda568a0a8e81ca7816a0e7168e
Fabian Rico Vargas <Fabian.D.Rico.Vargas@gds.ey.com>
2021-05-09 23:22:49 -0300

    Updated links

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -17,7 +17,7 @@ export class SearchComponent  {
     ]},
     {Name : "How-To", 
     Links: [ 
-      { title: 'Reach me out', description: 'You can send me a message through Likendin Chat', sourceLink: 'https://www.linkedin.com/in/fabian-daniel-rico-vargas-89731a107/' },
+      { title: 'Reach me out', description: 'You can send me a message through LinkedIn Chat', sourceLink: 'https://www.linkedin.com/in/fabian-daniel-rico-vargas-89731a107/' },
       { title: 'Modern WorkPlace', description: 'Set up your Mobile Devide', sourceLink: 'https://www.google.com' },
       { title: 'How to request External CE credits', description: ' Learn How to rquest CE credits from courses in internal websites', sourceLink: 'https://www.google.com' },
       { title: 'How to request VISA Home', description: 'Find the way to request access to your corporate credit card summary', sourceLink: 'https://www.google.com' }
